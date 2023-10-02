2c32daf047d99f3849b13cf910da86b9f749fec7
Lawrence DENEIPLE <l.deneiple@axelor.com>
2023-10-02 16:55:40 +0200

    style: apply spotless format

--- src/main/java/com/axelor/utils/service/ActionServiceImpl.java
+++ src/main/java/com/axelor/utils/service/ActionServiceImpl.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
 
