f605986727e592e1e834523a45a629445b707fbd
Lawrence Deneiple <l.deneiple@axelor.com>
2024-09-23 15:59:46 +0200

    style: apply spotless format

--- src/test/java/com/axelor/utils/service/AllModelSelectServiceTest.java
+++ src/test/java/com/axelor/utils/service/AllModelSelectServiceTest.java
@@ -2,7 +2,6 @@ package com.axelor.utils.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import com.axelor.events.StartupEvent;
 import com.axelor.meta.db.MetaModel;
 import com.axelor.meta.db.MetaSelect;
@@ -13,82 +12,75 @@ import com.axelor.meta.loader.LoaderHelper;
 import com.axelor.utils.junit.BaseTest;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
-import org.checkerframework.checker.units.qual.A;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class AllModelSelectServiceTest extends BaseTest {
 
-    private final MetaSelectRepository metaSelectRepo;
-    private final MetaModelRepository metaModelRepo;
-    private final LoaderHelper loaderHelper;
-    private final AllModelSelectService allModelSelectService;
+  private final MetaSelectRepository metaSelectRepo;
+  private final MetaModelRepository metaModelRepo;
+  private final LoaderHelper loaderHelper;
+  private final AllModelSelectService allModelSelectService;
 
-    @Inject
-    AllModelSelectServiceTest(
-            MetaSelectRepository metaSelectRepo,
-            MetaModelRepository metaModelRepo,
+  @Inject
+  AllModelSelectServiceTest(
+      MetaSelectRepository metaSelectRepo,
+      MetaModelRepository metaModelRepo,
+      LoaderHelper loaderHelper,
+      AllModelSelectService allModelSelectService) {
+    this.metaSelectRepo = metaSelectRepo;
+    this.metaModelRepo = metaModelRepo;
+    this.loaderHelper = loaderHelper;
+    this.allModelSelectService = allModelSelectService;
+  }
 
-            LoaderHelper loaderHelper,
-            AllModelSelectService allModelSelectService) {
-        this.metaSelectRepo = metaSelectRepo;
-        this.metaModelRepo = metaModelRepo;
-        this.loaderHelper = loaderHelper;
-        this.allModelSelectService = allModelSelectService;
+  @BeforeEach
+  void setUp() {
+    loaderHelper.importCsv("data/metamodel-input.xml");
+    loaderHelper.importCsv("data/metaselects-input.xml");
+    loaderHelper.importCsv("data/metaselectitems-input.xml");
+  }
+
+  @Test
+  @Transactional
+  void testCreateSelectionOfAllModels_NewSelect() {
+    List<MetaSelect> existingMetaSelect =
+        metaSelectRepo.all().filter("self.name = ?1", "all.model.reference.select").fetch();
+    if (existingMetaSelect != null) {
+      for (MetaSelect metaSelect : existingMetaSelect) {
+        metaSelectRepo.remove(metaSelect);
+      }
     }
+    assertNull(metaSelectRepo.findByName("all.model.reference.select"));
 
-    @BeforeEach
-    void setUp() {
-        loaderHelper.importCsv("data/metamodel-input.xml");
-        loaderHelper.importCsv("data/metaselects-input.xml");
-        loaderHelper.importCsv("data/metaselectitems-input.xml");
-    }
+    allModelSelectService.createSelectionOfAllModels(new StartupEvent());
 
-    @Test
-    @Transactional
-    void testCreateSelectionOfAllModels_NewSelect() {
-        List<MetaSelect> existingMetaSelect = metaSelectRepo.all().filter("self.name = ?1", "all.model.reference.select").fetch();
-        if (existingMetaSelect != null) {
-            for (MetaSelect metaSelect : existingMetaSelect) {
-                metaSelectRepo.remove(metaSelect);
+    MetaSelect metaSelectAfterAction = metaSelectRepo.findByName("all.model.reference.select");
 
-            }
-        }
-        assertNull(metaSelectRepo.findByName("all.model.reference.select"));
+    assertNotNull(metaSelectAfterAction);
+    assertEquals(metaModelRepo.all().fetch().size(), metaSelectAfterAction.getItems().size());
+  }
 
-        allModelSelectService.createSelectionOfAllModels(new StartupEvent());
+  @Test
+  void testCreateSelectionOfAllModels_ExistingSelect() {
+    allModelSelectService.createSelectionOfAllModels(new StartupEvent());
 
-        MetaSelect metaSelectAfterAction = metaSelectRepo.findByName("all.model.reference.select");
+    List<MetaModel> metaModelList = metaModelRepo.all().fetch();
+    MetaSelect metaSelect = metaSelectRepo.findByName("all.model.reference.select");
 
-        assertNotNull(metaSelectAfterAction);
-        assertEquals(metaModelRepo.all().fetch().size(), metaSelectAfterAction.getItems().size());
-    }
+    assertNotNull(metaSelect);
 
-    @Test
-    void testCreateSelectionOfAllModels_ExistingSelect() {
-        allModelSelectService.createSelectionOfAllModels(new StartupEvent());
+    Set<String> metaModelNames =
+        metaModelList.stream().map(MetaModel::getFullName).collect(Collectors.toSet());
 
-        List<MetaModel> metaModelList = metaModelRepo.all().fetch();
-        MetaSelect metaSelect = metaSelectRepo.findByName("all.model.reference.select");
+    Set<String> metaSelectItemValues =
+        metaSelect.getItems().stream().map(MetaSelectItem::getValue).collect(Collectors.toSet());
 
-        assertNotNull(metaSelect);
+    assertEquals(metaModelList.size(), metaSelect.getItems().size());
 
-        Set<String> metaModelNames = metaModelList.stream()
-                .map(MetaModel::getFullName)
-                .collect(Collectors.toSet());
-
-        Set<String> metaSelectItemValues = metaSelect.getItems().stream()
-                .map(MetaSelectItem::getValue)
-                .collect(Collectors.toSet());
-
-        assertEquals(metaModelList.size(), metaSelect.getItems().size());
-
-        assertEquals(metaModelNames, metaSelectItemValues);
-    }
-
+    assertEquals(metaModelNames, metaSelectItemValues);
+  }
 }
