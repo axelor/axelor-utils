6c7acd7e4306761cd077c978db653326cd48030f
David Trochel <d.trochel@axelor.com>
2025-01-11 01:22:27 +0100

    remove unused import

--- src/test/java/com/axelor/utils/context/ActionViewHelperTest.java
+++ src/test/java/com/axelor/utils/context/ActionViewHelperTest.java
@@ -9,7 +9,6 @@ import com.axelor.utils.helpers.context.ActionViewHelper;
 import com.axelor.utils.junit.BaseTest;
 import com.axelor.utils.utils.TestingHelper;
 import com.google.inject.Inject;
-import java.io.IOException;
 import javax.xml.bind.JAXBException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
