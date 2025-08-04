fafb51a7c9d7fe4a9532ea1ba266fcd18fa41c25
David Trochel <d.trochel@axelor.com>
2025-08-05 00:15:50 +0200

    refacto: Replace this usage of 'Stream.collect(Collectors.toList())' with 'Stream.toList()'

--- src/test/java/com/axelor/utils/context/EntityMergingHelperTest.java
+++ src/test/java/com/axelor/utils/context/EntityMergingHelperTest.java
@@ -23,7 +23,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -165,10 +164,8 @@ class EntityMergingHelperTest extends BaseTest {
     Move move = Query.of(Move.class).filter("self.code = 'TEST_MOVE_1'").fetchOne();
     Assertions.assertNotNull(move);
 
-    List<MoveLine> moveLines = move.getMoveLines();
-
     List<Map<String, Object>> moveLineMapList =
-        moveLines.stream().map(this::moveLineToMap).collect(Collectors.toList());
+        move.getMoveLines().stream().map(this::moveLineToMap).toList();
 
     moveLineMapList.forEach(moveLineMap -> moveLineMap.put("credit", BigDecimal.ONE));
 
