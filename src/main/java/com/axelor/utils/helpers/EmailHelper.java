300499e175e5bf085164eaabae21e65867b7a4cd
David Trochel <d.trochel@axelor.com>
2026-01-19 16:38:26 +0100

    fix(email): allow dashes in intermediate domain segments

--- src/main/java/com/axelor/utils/helpers/EmailHelper.java
+++ src/main/java/com/axelor/utils/helpers/EmailHelper.java
@@ -27,7 +27,7 @@ public class EmailHelper {
     }
 
     final String EMAIL_PATTERN =
-        "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
     Pattern pattern = Pattern.compile(EMAIL_PATTERN);
     return pattern.matcher(email).matches();
   }
